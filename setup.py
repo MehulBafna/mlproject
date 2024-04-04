275650e5d0f7b9455f7b1aff850d6a041446e50e
MehulBafna <mehulbafna198@gmail.com>
2024-04-04 15:25:41 +0200

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages,setup 
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return list of requirements.txt
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        [req.replace("\n","") for req in requirements]
+        
+        return requirements[0:len(requirements)-1]
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Mehul',
+    author_email='mehulbafna198@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
